2a7343dacf430126b93e478b13303d76db928b9f
Mayank dhakad <mayankirar@gmail.com>
2020-07-29 18:14:51 +0530

    Done With New Entry module Testing remaing

--- NewEntry/urls.py
+++ NewEntry/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 from django.urls import path
-from .views import EquipmentNewEntry, select_type, Equipmentautocomplete, Equipmentdetail
+from .views import NewEntry, EquipmentNewEntry, select_type, Equipmentautocomplete, Equipmentdetail
 
 
 urlpatterns = [
     path('select_type', select_type, name='select_type' ),
     path('<str:report_type>', EquipmentNewEntry, name="EquipmentNewEntry"),
     path('Equipmentdetail/<str:report_type>', Equipmentdetail, name="Equipmentdetail"),
+    path('add/<int:id>/<str:report_type>', NewEntry, name="NewEntry"),
     path('ajax/equipments', Equipmentautocomplete, name='Equipmentautocomplete'),
 
 ]
